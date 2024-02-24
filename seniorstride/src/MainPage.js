5c2888202c6ae12125b911c22ba921501b87127f
Ihana Fahmy <ihanafahmy@gmail.com>
2024-02-24 15:03:33 -0500

    Added the main headers

--- seniorstride/src/MainPage.js
+++ seniorstride/src/MainPage.js
@@ -8,8 +8,59 @@ const MainPage = () => {
       <img className="image" src={banner} alt="banner" />
 
       <h1>SeniorStride - Health Guide App</h1>
-      <h3>By Ihana Fahmy, Shamma Sarah Markis, Muiz Madadi</h3>
+      <p className="name">By Ihana Fahmy, Muiz Madadi, Shamma Sarah Markis</p>
+      <br />
+      <div className="content">
+        <h2>Introduction</h2>
+        <p>Insert text here</p>
 
+        <h2>Problem</h2>
+        <p>Insert text here</p>
+
+        <h2>Our solution</h2>
+        <p>Insert text here</p>
+
+        <h2>Research</h2>
+        <h3>Survey</h3>
+        <p>Insert text here</p>
+        <h3>Similar Apps</h3>
+        <p>Insert text here</p>
+
+        <h2>Personas</h2>
+        <p>Insert text here</p>
+
+        <h2>User Journeys</h2>
+        <p>Insert text here</p>
+
+        <h2>Design Ideas</h2>
+        <p>Insert text here</p>
+        <h3>Design Inpiration</h3>
+        <p>Insert text here</p>
+        <h3>Colour Palette</h3>
+        <p>Insert text here</p>
+        <h3>Typography</h3>
+
+        <h2>Sketches</h2>
+        <p>Insert text here</p>
+
+        <h2>Mockup</h2>
+        <p>Insert text here</p>
+
+        <h2>Final Design</h2>
+        <p>Insert text here</p>
+
+        <h2>Onboarding</h2>
+        <p>Insert text here</p>
+
+        <h2>Demo</h2>
+        <p>Insert text here</p>
+
+        <h2>Conclusion</h2>
+        <p>Insert text here</p>
+
+        <h2>References</h2>
+        <p>Insert text here</p>
+      </div>
     </>
   );
 };
